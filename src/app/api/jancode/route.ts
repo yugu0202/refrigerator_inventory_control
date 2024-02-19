8af31e7fee69a77f6f68d4b19243781839643772
yugu0202 <applerin1119@gmail.com>
2024-02-20 05:23:13 +0900

    Drawerの追加

--- src/app/api/jancode/route.ts
+++ src/app/api/jancode/route.ts
@@ -3,10 +3,24 @@ import { JSDOM } from 'jsdom';
 
 export async function GET(req: NextRequest) {
   const params = req.nextUrl.searchParams;
+
+  if (!params.has('code')) {
+    return NextResponse.json({name: 'codeパラメータが必要です'}, {status: 400})
+  }
+
+  const code = params.get('code');
+
+  if (code?.length !== 13 && !code?.match(/^\d+$/)) {
+    return NextResponse.json({name: 'JANコードが不正です'}, {status: 400})
+  }
+
   const response = await fetch(`https://jancode.xyz/code/?q=${params.get('code')}`)
   const text = await response.text()
   const dom = new JSDOM(text);
   const resultText = dom.window.document.querySelector('p.description')?.textContent?.trim();
 
-  return NextResponse.json({name: resultText || ''})
+  if (!resultText) {
+    return NextResponse.json({name: '商品が見つかりませんでした'}, {status: 404})
+  }
+  return NextResponse.json({name: resultText})
 };
--- src/app/barcode/page.tsx
+++ src/app/barcode/page.tsx
@@ -2,9 +2,60 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import { BrowserMultiFormatReader } from '@zxing/browser';
-import { ChakraProvider, useToast } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  useToast,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
 
-const BarcodeReaderPage = () => {
+
+function Scanedrawer() {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const [items, setItems] = useState<React.JSX.Element[]>([]);
+
+  useEffect(() => {
+    if (!isOpen) { return; }
+
+    let items: React.JSX.Element[] = [];
+
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+      if (key === null) { continue; }
+      const value = sessionStorage.getItem(key);
+      console.log(key, value);
+      items.push(<p key={key}>{value}</p>);
+    }
+
+    setItems(items);
+  }, [isOpen]);
+
+  return (
+    <>
+      <Button colorScheme='blue' onClick={onOpen} className="w-full lg:w-1/2">
+        &and;
+      </Button>
+      <Drawer placement="bottom" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>読み取り済み商品一覧</DrawerHeader>
+          <DrawerBody>
+            {items}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default function Page() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const codeReader = new BrowserMultiFormatReader();
   const toast = useToast();
@@ -15,20 +66,42 @@ const BarcodeReaderPage = () => {
   const startScanner = () => {
     if (!videoRef.current) { return; }
 
-    console.log(selectedDevice);
-
     codeReader.decodeFromVideoDevice(selectedDevice, videoRef.current, (code_result, error) => {
       if (code_result) {
-        fetch(`/api/jancode?code=${code_result.getText()}`).then((response) => response.json()).then((data) => {
+        console.log(sessionStorage.getItem(code_result.getText()));
+        if (sessionStorage.getItem(code_result.getText()) != null) { return; }
+
+
+        fetch(`/api/jancode?code=${code_result.getText()}`)
+        .then((response) => {
+          if (response.status === 400) {
+            return null
+          }
+
+          return response.json()
+        })
+        .then((data) => {
+          if (!data) { return; }
+
+          let status = "info" as "info" | "error" | "warning" | "success" | "loading" | undefined;
+
           console.log(data);
+
+          // ここは後々ユーザーに入力させるようにする
+          if (data.name === "商品が見つかりませんでした") {
+            status = "error";
+          }
+
           toast({
             title: '読み取り結果',
             description: data.name,
-            status: 'info',
+            status: status,
             position: 'top',
-            duration: 9000,
+            duration: 6000,
             isClosable: true,
           });
+
+          sessionStorage.setItem(code_result.getText(), data.name);
         });
       }
     });
@@ -52,25 +125,22 @@ const BarcodeReaderPage = () => {
       });
       setDevices(deviceOptions);
     });
-  });
+  }, []);
 
   return (
     <ChakraProvider>
+      <h1>バーコードリーダー</h1>
       <div>
-        <h1>バーコードリーダー</h1>
-        <div>
-          <button onClick={startScanner}>スキャン開始</button>
-          <button onClick={stopScanner}>スキャン停止</button>
-          <select onChange={handleDeviceChange}>
-            {devices}
-          </select>
-        </div>
-        <div style={{ width: '300px', margin: 'auto' }}>
-          <video ref={videoRef} />
-        </div>
+        <button onClick={startScanner}>スキャン開始</button>
+        <button onClick={stopScanner}>スキャン停止</button>
+        <select onChange={handleDeviceChange}>
+          {devices}
+        </select>
       </div>
+      <div style={{ width: '300px', margin: 'auto' }}>
+        <video ref={videoRef} />
+      </div>
+      <Scanedrawer />
     </ChakraProvider>
   );
 };
-
-export default BarcodeReaderPage;
